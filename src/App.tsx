927971d8cbad7ecc5b68c90831b04b94d5c46ed6
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-04-29 18:53:12 +0200

    Added multilanguage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,48 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Test from './1_fundaments/test/Test';
 import styled, { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+import { useTranslation } from "react-i18next";
+import i18n from "i18next";
+//import cookies from "js-cookie";
+const cookies = require('js-cookie');
 
+//for the light/dark theme
 const StyledApp = styled.div`
 
     color: ${(props) => props.theme.fontColor};
 
 `;
 
+const languages = [
+    {
+        code: 'fr',
+        name: 'Français',
+        country_code: 'fr'
+    },
+    {
+        code: 'en',
+        name: 'English',
+        country_code: 'gb',
+        dir: 'empty'
+    },
+]
+
 function App() {
+    const currentLanguageCode = cookies.get('i18next') || 'en'
+    const currentLanguage = languages.find(element => element.code === currentLanguageCode)
+    
+    const { t } = useTranslation()
+
+    const releaseDate = new Date('2021-03-07')
+    const timeDifference = new Date().valueOf() - releaseDate.valueOf()
+    const number_of_days = Math.floor(timeDifference / (1000 * 60 * 60 * 24))
+
+    useEffect(() => {
+        document.body.dir = currentLanguage!.dir || 'ltr';
+        document.title = t('app_title');
+    }, [currentLanguage, t])
+
     const [theme, setTheme] = useState("light");
 
     const themeToggler = () => {
@@ -21,11 +54,29 @@ function App() {
             <GlobalStyles />
             <StyledApp>
                 <div>
+                    <div className="dropdown">
+                        <button id="dropdownMenuButton1" type="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown button</button>
+                        <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                            <li><span>{t('language')}</span></li>
+                            {languages.map(({ code, name, country_code }) => (
+                                <li key={country_code}>
+                                    <button onClick={() => i18n.changeLanguage(code)}
+                                    disabled={code === currentLanguageCode}>
+                                        {/* <span className={`flag-icon flag-icon-${country_code}`}></span> */}
+                                        {name}
+                                    </button>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+
                     Hello world!
+                    {t('welcome_message')}
+                    <p>{t('days_since_release', { number_of_days })}</p>
 
                     <Test />
                 </div>
-                
+
                 <button onClick={() => themeToggler()}>Change theme</button>
             </StyledApp>
         </ThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,64 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.scss';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import HttpApi from 'i18next-http-backend';
+
+import './index.scss';
+//import 'flag-icon-css/css/flag-icon.min.css';
+import App from './App';
+
+i18n
+    .use(initReactI18next) // passes i18n down to react-i18next
+    .use(LanguageDetector)
+    .use(HttpApi)
+    .init({
+        // the translations
+        // (tip move them in a JSON file and import them,
+        // or even better, manage them via a UI: https://react.i18next.com/guides/multiple-translation-files#manage-your-translations-with-a-management-gui)
+
+        supportedLngs: ['en', 'fr'],
+
+        //lng: document.querySelector('html')!.lang, // if you're using a language detector, do not define the lng option
+        fallbackLng: "en",
+        detection: {
+            order: ['path', 'cookie', 'htmlTag', 'localStorage', 'subdomain'],
+            caches: ['cookie'],
+        },
+        backend: {
+            loadPath: 'assets/locales/{{lng}}/translation.json', //change to Laravel API later
+        },
+        //react: { useSuspense: false, }
+
+        /*interpolation: {
+            escapeValue: false // react already safes from xss => https://www.i18next.com/translation-function/interpolation#unescape
+        }*/
+    });
+
+/* function App() {
+    const { t } = useTranslation();
+
+    return <h2>{t('welcome_to_react')}</h2>; //t() is for the key of the content in translation
+} */
+
+/* show during loading */
+const loadingMarkup = (
+    <div>
+        <h2>Loading...</h2>
+    </div>
+)
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Suspense fallback={loadingMarkup}>
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    </Suspense>
 );
 
 // If you want to start measuring performance in your app, pass a function
