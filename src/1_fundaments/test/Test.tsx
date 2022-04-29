6969c73de1847a3c18a66a273328e7b249c261e5
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-04-29 11:40:00 +0200

    made scss setup

---
+++ src/1_fundaments/test/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './test.scss';
+
+function Test() {
+    return (
+        <div className="test__p">
+            This is not a test.
+        </div>
+    );
+}
+
+export default Test;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
-import React from 'react';
-//import logo from './logo.svg';
-//import './App.css';
+import React, { useState } from 'react';
+import Test from './1_fundaments/test/Test';
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+const StyledApp = styled.div`
+
+    color: ${(props) => props.theme.fontColor};
+
+`;
 
 function App() {
+    const [theme, setTheme] = useState("light");
+
+    const themeToggler = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
+
     return (
-        <div>
-            Hello world!
-        </div>
+        <ThemeProvider theme={theme === "light" ? lightTheme : darkTheme}>
+            <GlobalStyles />
+            <StyledApp>
+                <div>
+                    Hello world!
+
+                    <Test />
+                </div>
+                
+                <button onClick={() => themeToggler()}>Change theme</button>
+            </StyledApp>
+        </ThemeProvider>
     );
 }
 
