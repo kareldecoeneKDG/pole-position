c208a3dfea1a5631206b21a64c58b31cec24cddb
kareldecoeneKDG <64062257+kareldecoeneKDG@users.noreply.github.com>
2022-06-15 08:50:33 +0200

    push aspect ratio changes

--- src/4_organisms/Header/Header.tsx
+++ src/4_organisms/Header/Header.tsx
@@ -3,15 +3,8 @@ import { Link } from 'react-router-dom';
 
 import headerLogoLight from './Logo-big-light.png';
 import headerLogoDark from './Logo-big-dark.png';
-import placeholder from './placeholder.png';
 import backgroundRectangles from './bg-rectangles.png';
 import backgroundRectanglesDark from './bg-rectangles-dark.png';
-
-import mobilePreview from './mobile.webp';
-import tabletDarkPreview from './tablet-dark.webp';
-import tabletLightkPreview from './tablet-light.webp';
-import desktopDarkPreview from './desktop-dark.webp';
-//import desktopLightPreview from './desktop-light.webp';
 
 import { useTranslation } from "react-i18next";
 
@@ -24,6 +17,8 @@ function Header() {
             {/* BG RECTANGLES 1 */}
             <img className="bg-rect bg-rect1 light" src={backgroundRectangles} alt="Background rectangles illustration" />
             <img className="bg-rect bg-rect1 dark" src={backgroundRectanglesDark} alt="Background rectangles dark illustration" />
+
+
 
             {/* HEADER CONTENT */}
             <div className="header__container">
@@ -38,29 +33,57 @@ function Header() {
                     <span>{t('header_cta')} <i className="fa fa-chevron-right first-chevron" aria-hidden="true"></i><i className="fa fa-chevron-right" aria-hidden="true"></i></span>
                 </Link>
 
+
+
                 {/* <img className="header__img-placeholder" src={placeholder} alt="Full logo Pole Position" /> */}
 
-                {/* PREVIEWS */}
+                {/* PREVIEWS
                 <img className="header__img-placeholder mobile" src={mobilePreview} alt="Preview mobile Pole Position" />
                 <img className="header__img-placeholder tablet light" src={tabletLightkPreview} alt="Preview tablet Pole Position" />
-                <img className="header__img-placeholder tablet dark" src={tabletDarkPreview} alt="Preview tablet Pole Position" />
+                <img className="header__img-placeholder tablet dark" src={tabletDarkPreview} alt="Preview tablet Pole Position" /> */}
 
+                {/* MOBILE PREVIEW */}
+                <picture>
+                    <source srcSet="/images/pages/webp/mobile.webp" type="image/webp" />
+                    <source srcSet="/images/pages/jpg/mobile.jpg" type="image/jpg" />
+                    <img className="header__img-placeholder mobile" src="/images/pages/jpg/mobile.jpg" alt="Preview mobile Pole Position" />
+                </picture>
+
+                {/* TABLET PREVIEWS */}
+                <picture>
+                    <source srcSet="/images/pages/webp/tablet-light.webp" type="image/webp" />
+                    <source srcSet="/images/pages/jpg/tablet-light.jpg" type="image/jpg" />
+                    <img className="header__img-placeholder tablet light" src="/images/pages/jpg/desktop-light.jpg" alt="Preview tablet Pole Position" />
+                </picture>
+
+                <picture>
+                    <source srcSet="/images/pages/webp/tablet-dark.webp" type="image/webp" />
+                    <source srcSet="/images/pages/jpg/tablet-dark.jpg" type="image/jpg" />
+                    <img className="header__img-placeholder tablet dark" src="/images/pages/jpg/desktop-dark.jpg" alt="Preview tablet Pole Position" />
+                </picture>
+
+                {/* DESKTOP PREVIEWS */}
                 <picture>
                     <source srcSet="/images/pages/webp/desktop-light.webp" type="image/webp" />
                     <source srcSet="/images/pages/jpg/desktop-light.jpg" type="image/jpg" />
                     <img className="header__img-placeholder desktop light" src="/images/pages/jpg/desktop-light.jpg" alt="Preview desktop Pole Position" />
                 </picture>
 
-                <img className="header__img-placeholder desktop dark" src={desktopDarkPreview} alt="Preview desktop Pole Position" />
+                <picture>
+                    <source srcSet="/images/pages/webp/desktop-dark.webp" type="image/webp" />
+                    <source srcSet="/images/pages/jpg/desktop-dark.jpg" type="image/jpg" />
+                    <img className="header__img-placeholder desktop dark" src="/images/pages/jpg/desktop-dark.jpg" alt="Preview desktop Pole Position" />
+                </picture>
             </div>
 
-            {/* SCROLL DOWN ICON 
-            <Link className="header__scrolldown" to="latestnews">
-                <i className="header__scrolldown__icon fa fa-chevron-down" aria-hidden="true"></i>
-            </Link> */}
+
+
+            {/* SCROLL DOWN ICON */}
             <a className="header__scrolldown" href="#latestnews">
                 <i className="header__scrolldown__icon fa fa-chevron-down" aria-hidden="true"></i>
             </a>
+
+
 
             {/* BG RECTANGLES 2 */}
             <img className="bg-rect bg-rect2 light" src={backgroundRectangles} alt="Background rectangles illustration" />
